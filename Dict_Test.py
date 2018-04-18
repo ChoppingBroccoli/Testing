59bbdd86a8b377e033588bfea94165829a091744
Justin <jneezy2008@gmail.com>
2018-04-17 23:07:59 -0400

    Updated code and logic

--- Dict_Test.py
+++ Dict_Test.py
@@ -1,20 +1,25 @@
 prac_list = ['coin', 'dagger', 'sword']
-prac_dict = {'coin': 3, 'dagger': 2, 'sword':1, 'ruby': 1}
+prac_dict = {'coin': 23, 'dagger': 189, 'sword':1, 'ruby': 1}
+
+print('Here are the contents of the Dictionary (aka inventory):')
+print(prac_dict, '\n')
+
 
 for every_dict_item in prac_dict:
     prac_dict.get(every_dict_item, 0)
+    print('DEBUG FOR LOOP -- Dict item is ' + str(every_dict_item))
     for every_list_item in prac_list:
-        if every_list_item == every_dict_item:
-            print ('The item from the list matches an item from the dictionary')
-            print('List item was ', every_list_item)
-            print('Dict item was ', every_dict_item)
-
+        if every_list_item != every_dict_item:
+            print(str(every_list_item) + ' is in the List but not in your inventory')
+        else:
+            print('An item from the list (' + every_list_item + ') matches an item in your inventory (' + every_dict_item + ')')
+            print('You have ' + str(prac_dict[every_dict_item]) + ' of these in inventory')
+            
 
 '''
-Prints items from both the list and dict objects and does some checking. I want it to print the Dict and the List
-Want to adjust the print statements to better understand/see what the program is
- doing at each iteration.
+Cannot print information about 'ruby'. For loop ends with sword. Likely because the list
+does not have 'ruby' so it ends.
 
-Want to print each key-value pair before and after running through loops
+Need to find out why 'ruby' is not getting printed and add it to the Dict if it does not exist.
 
 '''
