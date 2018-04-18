e6525dbac527b611b7a506260f8f0a17cab294ca
Justin <jneezy2008@gmail.com>
2018-04-18 14:31:00 -0400

    Continued debugging. Items in the list are not properly being comared against items in the dictionary

--- Dict_Test.py
+++ Dict_Test.py
@@ -4,16 +4,20 @@ prac_dict = {'coin': 23, 'dagger': 189, 'sword':1, 'ruby': 1}
 print('Here are the contents of the Dictionary (aka inventory):')
 print(prac_dict, '\n')
 
-
-for every_dict_item in prac_dict:
-    prac_dict.get(every_dict_item, 0)
-    print('DEBUG FOR LOOP -- Dict item is ' + str(every_dict_item))
-    for every_list_item in prac_list:
-        if every_list_item != every_dict_item:
-            print(str(every_list_item) + ' is in the List but not in your inventory')
+list_it = 0
+for dict_item in prac_dict:
+    prac_dict.setdefault(dict_item, 0)
+    print('--DEBUG--')
+    print('Dictionary item is ' + str(dict_item))
+    print('We are comparing ' + str.upper(prac_list[list_it]) + ' from the List with ' + str.upper(dict_item) +
+          ' from the Dictionary')
+    list_it += 1
+    for list_item in prac_list:
+        if list_item != dict_item:
+            print(str.upper(list_item) + ',from the List, is not in your inventory')
         else:
-            print('An item from the list (' + every_list_item + ') matches an item in your inventory (' + every_dict_item + ')')
-            print('You have ' + str(prac_dict[every_dict_item]) + ' of these in inventory')
+            print(str.upper(list_item) + ', from the List matches ' + str.upper(dict_item) + ' in the Dictionary')
+            print('You have ' + str(prac_dict[dict_item]) + ' of these in inventory' + '\n')
             
 
 '''
